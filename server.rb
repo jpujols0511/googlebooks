b45585669579759b4bf8076a406f0a77e46b79fe
jpujols0511 <joanpujols1998@gmail.com>
2019-06-25 11:05:39 -0400

    Styled GoogleBooks, results displays the books and four pieces of info, working on the inner book container next

--- server.rb
+++ server.rb
@@ -1,18 +1,22 @@
 require "googlebooks"
 require "sinatra"
 
+get "/" do
+  erb :home
+end
+
+get "/home" do
+  erb :home
+end
+
 get "/results" do
-  theResult = GoogleBooks.search("#{params[:book]}")
-  bookResult = theResult.first
+  bookSearch = GoogleBooks.search("#{params[:book]}")
+  bookResult = bookSearch.first
 
   @bookAuthor = bookResult.authors
   @bookTitle = bookResult.title
   @bookID = bookResult.id
-  @bookISNB = bookResult.isnb
+  @bookISBN = bookResult.isbn
   @bookImage = bookResult.image_link
   erb :results
-end
-
-get "/" do
-  erb :home
 end
